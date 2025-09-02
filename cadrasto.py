fc9fcf2eb5cbb235e12e0df6dfe93468fdeeb7dc
murilokm12 <murilosantos2405@gmail.com>
2025-09-02 14:16:35 -0300

    cadrasto.py

--- cadrasto.py
+++ cadrasto.py
@@ -1,27 +1,29 @@
 listaUser=['jose','paulo']
 while True:
-    option=input('Digite uma opção:')
+    option=input('Digite uma opção:\n 1 para cadastrar o nome \n 2 para listar os nomes já cadastrados \n 3 para digitar o nome da lista que você quer \n excluir \n 0 para terminar o cadastro:')
+
+
 
     if option == '1' :
 
-     nome=input('Digite seu nome: ')
-
+     nome=input('Digite seu nome para o cadastro: ')
+     print('Nome cadastrado, para vê-lo, escolhe a opção 2')
      listaUser.append(nome)
 
     elif option == '2':
 
-     print(listaUser)  
+     print('Nomes cadastrados:',listaUser)  
 
     elif option =='3':
 
         nomeDelete=input('Digite o nome que quer deletar: ')
     
-        for item in listaUser:
-         if item == nomeDelete:
+         
+        if nomeDelete in listaUser:
           listaUser.remove(nomeDelete)
           print('Nome deletado')
         
-         else:
+        else:
            print('Não achei esse nome na lista!')
        
     elif option == '0':
