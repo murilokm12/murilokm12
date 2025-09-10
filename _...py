a31364ee24411e9e74d49fbb90fc28bc59732295
murilokm12 <murilosantos2405@gmail.com>
2025-09-10 16:16:52 -0300

    porcentagem.py

--- _...py
+++ _...py
@@ -1,7 +1,9 @@
-valor_total=  float(input("digite o valor total"))
-porcentagem =  float(input("digite a porcentagem"))
+valor_total=  float(input("digite o valor total: "))
+porcentagem =  float(input("digite a porcentagem: "))
 
 valor_parte= valor_total + (porcentagem / 100)
 
-print(f"{porcentagem}% de {valor_total}é:{valor_parte"})
+print(f"{porcentagem}% de {valor_total}é:{valor_parte}")
+
+
 
